53e14b226dd59d652da76a858c1e55236dd46f31
NoneOfYourBusiness <tomas0427aw@gmail.com>
2015-02-26 20:50:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@
     /*function kill() {}*/
     
     function loadUI() {
-        $('.room-background').css('background','url("https://i.imgur.com/N82wzhY.png")');
+        $('.room-background').css('background','url("http://i.imgur.com/N82wzhY.png")');
         $('head').append('<style type="text/css">body {background: url("http://i.imgur.com/1aw3xcd.png");background-repeat: no-repeat;background-size: cover;}#iBot {background: none repeat scroll 0% 0% rgba(28, 31, 37, 0.7);min-width: 180px;min-height: 165px;position: absolute;top: 54px;left: 0;}#brand {opacity: 0.9;width: 75px;height: 50px;background: #499FDA;}#brand h3 {margin: 0;padding: 12px 0;font-family: Arial;font-weight: bold;font-size: 20px;text-align: center;color: #FFF3FC;cursor: default;}</style>');
         $('body').append('<div id="iBot"><div id="brand"><h3>iBot</h3></div></div>');
     }
@@ -26,7 +26,7 @@
         if (plugMe.status == true) {
             setTimeout(function() {
                 $("brand").css("background", "#A9D133");
-            }, 1000);
+            }, 2000);
         }
     }
     
