49407420bf003ac77484025427e8b059449c1ca2
NoneOfYourBusiness <tomas0427aw@gmail.com>
2015-02-26 21:16:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,16 +5,26 @@
         name: "PlugMe",
         version: "0.1",
         UI: true,
-        settings: {}
+        settings: {
+            show: true,
+            autowoot: true
+        },
+        data: {}
     };
     
     /*function kill() {}*/
     
     function loadUI() {
         $('.room-background').css('background','url("http://i.imgur.com/N82wzhY.png")');
-        $('head').append('<style type="text/css">body {background: url("http://i.imgur.com/1aw3xcd.png");background-repeat: no-repeat;background-size: cover;}#iBot {background: none repeat scroll 0% 0% rgba(28, 31, 37, 0.7);min-width: 180px;min-height: 165px;position: absolute;top: 54px;left: 0;}#brand {opacity: 0.9;width: 75px;height: 50px;background: #499FDA;}#brand h3 {margin: 0;padding: 12px 0;font-family: Arial;font-weight: bold;font-size: 20px;text-align: center;color: #FFF3FC;cursor: default;}</style>');
+        $('head').append('<style type="text/css">body {background: url("http://i.imgur.com/1aw3xcd.png");background-repeat: no-repeat;background-size: cover;}#iBot {background: none repeat scroll 0% 0% rgba(28, 31, 37, 0.85);min-width: 180px;min-height: 165px;position: absolute;top: 54px;left: 0;}#brand {width: 53px;height: 50px;background: #499FDA;}#brand h3 {margin: 0;padding: 12px 0;font-family: Arial;font-weight: bold;font-size: 20px;text-align: center;color: #FFF3FC;cursor: default;}</style>');
         $('body').append('<div id="iBot"><div id="brand"><h3>iBot</h3></div></div>');
     }
+    
+    function callEvents() {
+        if (plugMe) { woot(); }
+    }
+    
+    function woot() { $('#woot').click(); }
     
     function startup() {
         if (plugMe.UI) {
@@ -28,6 +38,8 @@
                 $("#brand").css("background", "#A9D133");
             }, 2000);
         }
+        
+        callEvents();
     }
     
     startup();
